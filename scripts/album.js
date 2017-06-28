b84ca20554e54404e7374b38e2e7d4f79be2c329
slidar <slidarv@yahoo.com>
2017-06-27 18:28:57 -0700

    not complete

--- scripts/album.js
+++ scripts/album.js
@@ -96,4 +96,30 @@ var albumImage = document.getElementsByClassName('album-cover-art')[0];
 
        setCurrentAlbum(albums[i]);
      });
+
+     var findParentByClassName = function(element, album)
+      if (element) {
+        var currrentParent = element.parentElement;
+        while (currrentParent.className !== album && currentParent.album !== null){
+          currentParent = currentParent.parentElement;
+        }
+        return currentParent;
+      }
+    var getSongItem = function(element) {
+      switch (elemt.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+          return findParentByClassName(element, 'song-item-number'):
+        case 'album-view-song-item':
+          return element.querySelector('.song-item-number'):
+        case 'song-item-title':
+        case 'song-item-duration':
+          return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number'):
+        case 'song-item-number':
+          return element;
+        default:
+            return;
+      }
+    };
  };
